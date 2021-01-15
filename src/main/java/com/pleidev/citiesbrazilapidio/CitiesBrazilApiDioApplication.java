c62c2206a4ac65b6e99cbb78ed7d3ef97d5d1862
pleiterson <pleiterson@gmail.com>
2021-01-15 11:18:46 -0300

    first commit

---
+++ src/main/java/com/pleidev/citiesbrazilapidio/CitiesBrazilApiDioApplication.java
@@ -0,0 +1,13 @@
+package com.pleidev.citiesbrazilapidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesBrazilApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesBrazilApiDioApplication.class, args);
+	}
+
+}
